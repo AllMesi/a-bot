fb09bebc2abe66c5b563be8f1b24f8af1fcf3215
AllMesi <sketchgamermusic100az@gmail.com>
2023-07-19 18:04:17 +1000

    test

---
+++ evalCodeTemplates/colour fill.js
@@ -0,0 +1,21 @@
+const d = [1280, 720];
+
+const { Image } = require("imagescript");
+const { AttachmentBuilder } = require("discord.js");
+
+const image = new Image(d[0], d[1]);
+image.fill(() => Math.floor(0xffffffff * Math.random()));
+
+for (const [x, y, color] of image.iterateWithColors()) {
+  const [r, g , b, opacity] = Image.colorToRGBA(color);
+  if (opacity === 0) image.setPixelAt(x, y, 0x00000000);
+  else image.setPixelAt(x, y, Image.rgbaToColor(r, g, b, 255))
+}
+
+image.encode().then(array => {
+  const buffer = Buffer.from(array);
+  const attachment = new AttachmentBuilder(buffer, { name: 'thingy.png' });
+  interaction.followUp({
+    files: [attachment]
+  });
+});
